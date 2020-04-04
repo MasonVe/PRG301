d04439f21ee5933711471698e2228ff47f8e9fef
MasonVe <102722004@student.swin.edu.au>
2020-04-05 05:03:32 +1000

    Added 2.4 Task

---
+++ Nested Repetition/NestedRepetition-Ex2.4/NestedRepetition-Ex2.4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NestedRepetition_Ex2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = "x";
+            int counter = 1;
+            int counter2 = 0;
+            int desired = 0;
+
+            Console.Write("Enter Desired Amount To Print: ");
+            desired = int.Parse(Console.ReadLine());
+            desired = (desired + 1);
+            while (counter < desired)
+            {
+
+                while (counter2 != counter)
+                {
+                    Console.Write(type);
+                    counter2++;
+                }
+
+                counter++;
+                counter2 = 0;
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
