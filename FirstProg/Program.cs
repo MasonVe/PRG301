1c763ea9402eb6e970e21fda8f92e8a4f74bee80
MasonVe <102722004@student.swin.edu.au>
2020-04-05 05:09:50 +1000

    Added First Program

---
+++ FirstProg/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Programming
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your first name?");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("What's your last name?");
+            string lastname = Console.ReadLine();
+            Console.WriteLine("Hello " + firstname + " " + lastname + "!");
+            Console.ReadLine();
+            
+        }
+    }
+}
